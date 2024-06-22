fe8efa4999fa271e51a249da27af68f2ae53950c
g0en <franco.barroso@alumnos.frm.utn.edu.ar>
2024-06-22 14:53:38 -0300

    cambios insumo

--- src/screens/ArticuloInsumoList.tsx
+++ src/screens/ArticuloInsumoList.tsx
@@ -1,12 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, IconButton } from "@mui/material";
+import { Edit, Visibility, Delete, Check } from "@mui/icons-material";
 import SideBar from "../components/common/SideBar";
 import { CloudinaryUpload } from "../services/CloudinaryService";
 import { CloudinaryDelete } from "../services/CloudinaryService";
+import ArticuloInsumo from "../types/ArticuloInsumo";
+import { ArticuloInsumoFindBySucursal } from "../services/ArticuloInsumoService";
+import { useParams } from "react-router-dom";
 
 function ArticuloInsumoList() {
+    const [articulosInsumo, setArticulosInsumo] = useState<ArticuloInsumo[]>([]);
+    const { idSucursal } = useParams();
     const [file, setFile] = useState<File | null>(null);
     const [uploadedUrl, setUploadedUrl] = useState<string | null>(null);
     const [publicId, setPublicId] = useState<string | null>(null);
+
+    const getAllArticuloInsumoBySucursal = async () => {
+        const articulosInsumo: ArticuloInsumo[] = await ArticuloInsumoFindBySucursal(Number(idSucursal));
+        setArticulosInsumo(articulosInsumo);
+    };
 
     const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         if (event.target.files) {
@@ -38,9 +50,60 @@ function ArticuloInsumoList() {
         }
     };
 
-    return (
+    useEffect(() => {
+        getAllArticuloInsumoBySucursal();
+    }, []);
 
-        <div>
+    return (
+        <>
+            <SideBar></SideBar>
+            <h6>Articulos Insumos</h6>
+            <TableContainer component={Paper}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Nombre</TableCell>
+                            <TableCell>Precio Compra</TableCell>
+                            <TableCell>Precio Venta</TableCell>
+                            <TableCell>Unidad de Medida</TableCell>
+                            <TableCell>Stock Actual</TableCell>
+                            <TableCell>Stock Mínimo</TableCell>
+                            <TableCell>Stock Máximo</TableCell>
+                            <TableCell>Para Elaborar</TableCell>
+                            <TableCell>Categoría</TableCell>
+                            <TableCell>Acciones</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {articulosInsumo.map((articulo) => (
+                            <TableRow key={articulo.id}>
+                                <TableCell>{articulo.denominacion}</TableCell>
+                                <TableCell>{articulo.precioCompra}</TableCell>
+                                <TableCell>{articulo.precioVenta}</TableCell>
+                                <TableCell>{articulo.unidadMedida.denominacion}</TableCell>
+                                <TableCell>{articulo.stockActual}</TableCell>
+                                <TableCell>{articulo.stockMinimo}</TableCell>
+                                <TableCell>{articulo.stockMaximo}</TableCell>
+                                <TableCell>
+                                    {articulo.esParaElaborar ? <Check color="success" /> : ""}
+                                </TableCell>
+                                <TableCell>{articulo.categoria.denominacion}</TableCell>
+                                <TableCell>
+                                    <IconButton aria-label="edit">
+                                        <Edit />
+                                    </IconButton>
+                                    <IconButton aria-label="view">
+                                        <Visibility />
+                                    </IconButton>
+                                    <IconButton aria-label="delete">
+                                        <Delete />
+                                    </IconButton>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
             <input type="file" onChange={handleFileChange} />
             <button onClick={handleUpload}>Upload</button>
             {uploadedUrl && (
@@ -49,7 +112,7 @@ function ArticuloInsumoList() {
                     <button onClick={handleDelete}>Delete</button>
                 </div>
             )}
-        </div>
+        </>
     );
 }
 
--- src/services/ArticuloInsumoService.ts
+++ src/services/ArticuloInsumoService.ts
@@ -1,6 +1,6 @@
 import ArticuloInsumo from "../types/ArticuloInsumo";
 
-export async function CategoriaCreate(articuloInsumo: ArticuloInsumo){
+export async function ArticuloInsumoCreate(articuloInsumo: ArticuloInsumo){
 	const urlServer = 'http://localhost:8080/articuloInsumo';
 	const response = await fetch(urlServer, {
 		method: 'POST',
@@ -14,7 +14,20 @@ export async function CategoriaCreate(articuloInsumo: ArticuloInsumo){
 	return await response.json() as ArticuloInsumo;
 }
 
-export async function CategoriaGetAll(){
+export async function ArticuloInsumoFindBySucursal(id: number){
+	const urlServer = 'http://localhost:8080/articuloInsumo/findBySucursal/' + id;
+	const response = await fetch(urlServer, {
+		method: 'GET',
+        headers: {
+			'Content-type': 'application/json',
+			'Access-Control-Allow-Origin':'*'
+		},
+        mode: 'cors'
+	});
+	return await response.json() as ArticuloInsumo[];
+}
+
+export async function ArticuloInsumoGetAll(){
 	const urlServer = 'http://localhost:8080/articuloInsumo';
 	const response = await fetch(urlServer, {
 		method: 'GET',
@@ -27,7 +40,7 @@ export async function CategoriaGetAll(){
 	return await response.json() as ArticuloInsumo[];
 }
 
-export async function CategoriaGetById(id: number){
+export async function ArticuloInsumoGetById(id: number){
 	const urlServer = 'http://localhost:8080/articuloInsumo/' + id;
 	const response = await fetch(urlServer, {
 		method: 'GET',
@@ -40,7 +53,7 @@ export async function CategoriaGetById(id: number){
 	return await response.json() as ArticuloInsumo;
 }
 
-export async function CategoriaUpdate(articuloInsumo: ArticuloInsumo){
+export async function ArticuloInsumoUpdate(articuloInsumo: ArticuloInsumo){
 	const urlServer = 'http://localhost:8080/articuloInsumo/' + articuloInsumo.id;
 	const response = await fetch(urlServer, {
 		method: 'PUT',
